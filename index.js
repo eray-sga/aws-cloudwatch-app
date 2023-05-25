6a0828623742369c0d374a343f88a2c7abce65f9
Eray Kalkan <ery.kalkan19@outlook.com>
2023-05-25 16:20:51 +0300

    all request logs
    all request logs

--- index.js
+++ index.js
@@ -6,45 +6,47 @@ dotenv.config();
 
 const app = express();
 
-app.listen(5001, () => {
-    console.log("Server is running on port 5001");
-})
-
-app.get("/", (req, res) => {
-    res.json({ message: "Welcome to my application" });
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: 'us-west-2'
 });
 
-app.get("/logs", (req, res) => {
+const cloudwatchLogs = new AWS.CloudWatchLogs();
 
-    AWS.config.update({
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-        region: 'us-west-2'
-    });
+function logRequests(req, res, next) {
+  const logGroupName = 'NODE_APP';
+  const logStreamName = 'NODE_APP_STREAM';
+  const logMessage = `[${new Date().toISOString()}] ${req.method} ${req.url} ${req.get('user-agent')} ${req.get('x-forwarded-for')}`;
 
+  const params = {
+    logGroupName: logGroupName,
+    logStreamName: logStreamName,
+    logEvents: [
+      {
+        message: logMessage,
+        timestamp: new Date().getTime()
+      }
+    ]
+  };
 
-    const cloudwatchLogs = new AWS.CloudWatchLogs();
+  cloudwatchLogs.putLogEvents(params, function(err, data) {
+    if (err) {
+      console.error('Log gönderimi başarısız:', err);
+    } else {
+      console.log('Log başarıyla gönderildi:', data);
+    }
+  });
 
-    function sendLogMessage(logGroupName, logStreamName, logMessage) {
-    const params = {
-        logGroupName: logGroupName,
-        logStreamName: logStreamName,
-        logEvents: [
-        {
-            message: logMessage,
-            timestamp: new Date().getTime()
-        }
-        ]
-    };
-
-    cloudwatchLogs.putLogEvents(params, function(err, data) {
-        if (err) {
-            res.json({message: "Log gönderimi başarısız", err})
-        } else {
-            res.json({message: "Log başarıyla gönderildi", data})
-        }
-    });
+  next();
 }
 
-    sendLogMessage('NODE_APP', 'NODE_APP_STREAM', 'nodejs uygulamasından gelen log.');
+app.use(logRequests);
+
+app.listen(5001, () => {
+  console.log("Server is running on port 5001");
+});
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to my application" });
 });
