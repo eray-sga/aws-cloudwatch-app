96da2e0169a3f67c29c36765d0e23f25504aba99
Eray Kalkan <ery.kalkan19@outlook.com>
2023-05-25 15:56:58 +0300

    region updated
    region updated

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.get("/logs", (req, res) => {
     AWS.config.update({
         accessKeyId: process.env.AWS_ACCESS_KEY_ID,
         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-        region: process.env.AWS_REGION
+        region: process.env.REGION
     });
 
 
