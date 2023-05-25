83f8af8792273645e93490f393b7e6d269b1dc0c
Eray Kalkan <ery.kalkan19@outlook.com>
2023-05-25 15:51:11 +0300

    aws cloudwacth logs
    aws cloudwacth logs integration

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const express = require("express");
+const AWS = require('aws-sdk');
+
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
 
@@ -8,4 +12,39 @@ app.listen(5001, () => {
 
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to my application" });
+});
+
+app.get("/logs", (req, res) => {
+
+    AWS.config.update({
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        region: process.env.AWS_REGION
+    });
+
+
+    const cloudwatchLogs = new AWS.CloudWatchLogs();
+
+    function sendLogMessage(logGroupName, logStreamName, logMessage) {
+    const params = {
+        logGroupName: logGroupName,
+        logStreamName: logStreamName,
+        logEvents: [
+        {
+            message: logMessage,
+            timestamp: new Date().getTime()
+        }
+        ]
+    };
+
+    cloudwatchLogs.putLogEvents(params, function(err, data) {
+        if (err) {
+            res.json({message: "Log gönderimi başarısız", err})
+        } else {
+            res.json({message: "Log başarıyla gönderildi", data})
+        }
+    });
+}
+
+    sendLogMessage('NODE_APP', 'NODE_APP_STREAM', 'nodejs uygulamasından gelen log.');
 });
